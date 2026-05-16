b1d43b1d1aed49408a83bd89ef38c007194c31b8
danh-real <htduyanh210604@gmail.com>
2026-05-16 05:28:23 +0000

    fix distributed train bash script

--- train/train/script/train_moe.sh
+++ train/train/script/train_moe.sh
@@ -10,4 +10,4 @@ export XFL_CONFIG=./train/config/${CONFIG_FILE}
 echo "Using config: $XFL_CONFIG"
 export TOKENIZERS_PARALLELISM=true
 export PYTHONPATH=.
-CUDA_VISIBLE_DEVICES=0 accelerate launch --main_process_port ${PORT} -m src.train.train_moe
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port ${PORT} -m src.train.train_moe
